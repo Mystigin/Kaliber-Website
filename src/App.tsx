06f64e4565f8379d3eb2a7873bcb4a6c897f21b7
Mystigin <karlkies22@gmail.com>
2026-04-20 23:42:50 -0400

    Scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Demo from "./pages/Demo";
 import Register from "./pages/Register";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/demo" element={<Demo />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/demo" element={<Demo />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
